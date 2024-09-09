8bc3c6467be7ef75e2decfd830ffafd2dd159e72
Abhirup2408 <133313748+Abhirup2408@users.noreply.github.com>
2024-09-09 22:26:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from spellchecker import SpellChecker
 nlp = spacy.load("en_core_web_trf")
 
 # Load the dataset
-countries = pd.read_csv(r"c:\Users\ACER\Downloads\countries.csv") 
+countries = pd.read_csv(r"countries.csv") 
 countries["category"] = "country"
 countries["name"] = countries["name"].apply(lambda x: x.lower())
 

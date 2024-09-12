eb2b4c374f6adaf62d2b61e38430da957fff89fa
Abhirup2408 <133313748+Abhirup2408@users.noreply.github.com>
2024-09-12 21:04:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import spacy
 from spellchecker import SpellChecker
 import streamlit as st
 
+import os
+os.environ["THINC_API"] = "numpy"
+
 # Load the spaCy model
 nlp = spacy.load("en_core_web_trf")
 
